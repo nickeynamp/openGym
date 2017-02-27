00391281eb6926556bcd11253ddc34b0dc8df258
Nick <namp@kth.se>
2017-02-27 20:13:03 +0100

    rename and simple stuff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ScreenOrientation} from 'ionic-native';
 import { BluetoothSerial } from 'ionic-native';
-import * as $ from "jquery";
+import * as $ from 'jquery';
 
 ScreenOrientation.lockOrientation('landscape');
 
@@ -13,19 +13,19 @@ ScreenOrientation.lockOrientation('landscape');
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    let UUID: string = "20:16:10:10:18:31";
+    let MAC: string = "20:16:10:10:18:31";
     BluetoothSerial.isEnabled().then(res =>{
-      BluetoothSerial.connect(UUID); // not sure if it works, can someone check how to deal with obsevable ?
+      BluetoothSerial.connect(MAC); // not sure if it works, can someone check how to deal with obsevable ?
       BluetoothSerial.isConnected().then(success,fail); // needs testing, not sure if works
 
     }).catch(res => {
             console.log('Fail!');
-            $("div.t1").text('It does not work');
+            $("#energy").text('It does not work');
             });
 
     function success(){
       var data = "connected ++" ;
-      $("div.t1").text('data');
+      $("#energy").text('data');
 
 
       //BluetoothSerial.read(function(data){console.log(data);},fail);
