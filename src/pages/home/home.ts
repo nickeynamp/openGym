a6c962a5ad6d0758844325e6a6abab9b876b1db6
KarasMI7 <steven.zhou930605@gmail.com>
2017-02-27 15:55:09 +0100

    MAC adds

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,7 +13,7 @@ ScreenOrientation.lockOrientation('landscape');
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    let UUID: string = "mac_address";
+    let UUID: string = "20:16:10:10:18:31";
     BluetoothSerial.isEnabled().then(res =>{
       BluetoothSerial.connect(UUID); // not sure if it works, can someone check how to deal with obsevable ?
       BluetoothSerial.isConnected().then(success,fail); // needs testing, not sure if works
@@ -23,8 +23,12 @@ export class HomePage {
             });
 
     function success(){
-      console.log("Operation Succeeded");
-      var reading = BluetoothSerial.read();
+
+      //console.log("Operation Succeeded");
+      alert("success");
+      var data;
+      //BluetoothSerial.read(function(data){console.log(data);},fail);
+
     };
     function fail(){
       console.log("Operation Failed. Have you tried turning it on and off again?? ;)s");
