85a388db0b07a865bac402e796143f20b141a4d5
KarasMI7 <steven.zhou930605@gmail.com>
2017-02-27 18:42:12 +0100

    testing bluetooth
    .Connect didn’t work, need to figure out why

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,9 +13,12 @@ ScreenOrientation.lockOrientation('landscape');
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    let UUID: string = "20:16:10:10:18:31";
+    let MAC_bluetooth_module: string = "20:16:10:10:18:31";
+    let MAC_Adurino: string = "20:";
     BluetoothSerial.isEnabled().then(res =>{
-      BluetoothSerial.connect(UUID); // not sure if it works, can someone check how to deal with obsevable ?
+      BluetoothSerial.list().then(val=>{ $("div.t1").text(val);},error=>{console.log("error")});
+      BluetoothSerial.connect(MAC_bluetooth_module); // not sure if it works, can someone check how to deal with obsevable ?
+      $("div.t1").text('connected');
       BluetoothSerial.isConnected().then(success,fail); // needs testing, not sure if works
 
     }).catch(res => {
@@ -23,7 +26,7 @@ export class HomePage {
             $("div.t1").text('It does not work');
             });
 
-    function success(){
+    var success = function s1(){
       var data = "connected ++" ;
       $("div.t1").text('data');
 
