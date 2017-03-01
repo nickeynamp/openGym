a79b779c2e369f7a24f815f06a3af34f4bf4f41d
Nick <namp@kth.se>
2017-03-01 14:46:20 +0100

    idk what i am doing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,49 +12,55 @@ ScreenOrientation.lockOrientation('landscape');
 })
 export class HomePage {
   public arduino;
+  public mac_arduino = "20:16:10:10:18:31";
   constructor(public navCtrl: NavController) {
   
   }
 
-  connect(){
-    // BluetoothSerial.enable();
+  listDevices(){
+    BluetoothSerial.enable();
     //IS Bluetooth TURNED ON?
     BluetoothSerial.isEnabled().then(res =>{
       //BluetoothSerial.list().then(val=>{ $("#energy").text(val);},error=>{console.log("error")});
-
       //LIST DEVICES
       BluetoothSerial.list().then(
         (devices) => {
             // set the list to returned value
             $("#energy").html('Listing devices \n' +devices.length +devices);
             console.log("tracking here");
-            console.log("Info device is here" + devices[0]);
-            this.arduino = devices[4].id;
+            console.log("Info device is here", devices[0]);
+            devices.forEach(function(device){
+              if(device.id === this.mac_arduino){
+                this.connect();
+              }
+            })
             if(devices.length ===0){
               $("#energy").html("No Bluetooth Device found");
             }
-            
         });
-
-      //ATTEMP TO CONNECT
-      var deviceReady = function() {
-          $("#energy").html("Connected Hooooray");
-          BluetoothSerial.subscribeRawData();
-          BluetoothSerial.read().then(
-            data =>{
-              $("#energy").html(data);
-            }
-          );
-        };
-      var error = function(){
-            // $("#energy").html('Fail to connect to Arduino. Have you tried turning it on and off again??');
-          };
-      BluetoothSerial.connect(this.arduino);
-      BluetoothSerial.isConnected().then(deviceReady,error);
     }).catch(fail => {
             console.log('Fail!');
             $("#energy").html('Bluetooth is not enabled/supported');
             });
+  }
+
+  connect(){
+    //ATTEMP TO CONNECT
+    this.arduino = this.mac_arduino;
+    var deviceReady = function() {
+        $("#energy").html("Connected Hooooray");
+        BluetoothSerial.subscribeRawData();
+        BluetoothSerial.read().then( 
+          data =>{
+            $("#energy").html(data);
+          }
+        );
+      };
+    var error = function(){
+            // $("#energy").html('Fail to connect to Arduino. Have you tried turning it on and off again??');
+        };
+    BluetoothSerial.connect(this.arduino);
+    BluetoothSerial.isConnected().then(deviceReady,error);
   }
 
 }
